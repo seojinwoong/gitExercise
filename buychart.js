2d5d70baf68d8f9b62949b6721e9cbe86182716e
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2022-12-12 10:35:01 +0900

    Update buychart.js

--- buychart.js
+++ buychart.js
@@ -3,3 +3,6 @@ document.querySelector('.로컬');
 document.querySelector('테스트 브랜치에서 수정한 코드')
 document.querySelector('테스트 브랜치에서 수정한 코드2222')
 document.querySelector('테스트 브랜치에서 수정한 코드3333')
+document.querySelector('테스트 브랜치에서 수정한 코드4444')
+document.querySelector('테스트 브랜치에서 수정한 코드5555')
+
